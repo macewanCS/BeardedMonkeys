cd907438b45542c081eac645aaa228f9ee9ac649
Hafiz Temuri <talha.temuri@live.co.uk>
2017-01-26 15:31:32 -0500

    EPL commit

--- src/c395demo/c395demo/settings.py
+++ src/c395demo/c395demo/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'c395demo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['demo/templates'],
+        'DIRS': ['demo/templates', 'epl/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
--- src/c395demo/c395demo/urls.py
+++ src/c395demo/c395demo/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^demo/', include('demo.urls')),
+    url(r'^epl/', include('epl.urls')),
     url(r'^admin/', admin.site.urls),
 ]
