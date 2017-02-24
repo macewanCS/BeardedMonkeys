8c0585a665e9d95c2a8c0d00b2cc06c24158db07
Hafiz Temuri <talha.temuri@live.co.uk>
2017-02-23 21:41:12 -0500

    user authentication completed

--- src/c395demo/epl/urls.py
+++ src/c395demo/epl/urls.py
@@ -8,9 +8,11 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^hardware/', views.hardware, name='hardware'),
     url(r'^software/', views.software, name='software'),
-    url(r'^login/', views.login_view, name='login'),
     url(r'^manage-tickets/', views.manage, name='manage'),
     url(r'^general/', views.general, name='general'),
     url(r'^password-recovery/', views.password, name='password'),
     url(r'^service-request/', views.service, name='service'),
+    
+    url(r'^login/', views.login_view, name='login'),
+    url(r'^logout/', views.logout_view, name='logout'),
 ]
