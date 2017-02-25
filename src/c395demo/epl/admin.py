f62aad8646f02eb632eed7dd3bc38f196dd45408
mohammed alhamood <mnh20@live.com>
2017-02-24 21:40:12 -0500

    creat tickit update

--- src/c395demo/epl/admin.py
+++ src/c395demo/epl/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from epl.models import CallLog
+from epl.models import HardwareTicket, SoftwareTicket
 admin.site.register(CallLog)
+admin.site.register(HardwareTicket)
+admin.site.register(SoftwareTicket)
